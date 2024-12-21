c0317fc1d9db0d5e162160b91244b1eabc2b25d6
Dumitru <119630796+DiacencoDumitru@users.noreply.github.com>
2024-12-21 09:57:16 +0200

    Initial commit

---
+++ src/test/java/springcourse/practice/FirstSecurityApp/FirstSecurityAppApplicationTests.java
@@ -0,0 +1,13 @@
+package springcourse.practice.FirstSecurityApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSecurityAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
